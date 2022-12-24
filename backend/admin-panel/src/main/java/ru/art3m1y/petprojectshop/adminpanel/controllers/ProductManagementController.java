43a3714846754e4527a0bbe68a59237c5d865f73
Artem Kuryshkin <artem.kuryshkin@gmail.com>
2022-12-24 14:06:10 +0300

    initial commit

---
+++ backend/admin-panel/src/main/java/ru/art3m1y/petprojectshop/adminpanel/controllers/ProductManagementController.java
@@ -0,0 +1,9 @@
+package ru.art3m1y.petprojectshop.adminpanel.controllers;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/admin/products")
+public class ProductManagementController {
+
+}
